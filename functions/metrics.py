157c0474d03447357856c513c22855f631eadb09
Evan M <evanmm3@illinois.edu>
2023-12-07 01:11:09 -0600

    polishing utility/metric functions

--- functions/metrics.py
+++ functions/metrics.py
@@ -1,24 +1,24 @@
 import numpy as np
 
+
 """
 Calculates the mean squared difference per pixel between two images of the same shape.
 Lower MSE indicates more similar images.
 With perfect similarity, MSE=0.
 """
-def mse(im1, im2):
-    error = np.sum((im1.astype(float) - im2.astype(float)) ** 2)
+def mse(im1: np.ndarray, im2: np.ndarray) -> np.float64:
+    error: np.float64 = np.sum((im1.astype(float) - im2.astype(float)) ** 2)
     error /= im1.shape[0] * im1.shape[1]
-        
     return error
+
 
 """
 Calculates the root mean squared difference per pixel between two images of the same shape.
 Lower RMSE indicates more similar images.
 With perfect similarity, RMSE=0.
 """
-def rmse(im1, im2):
-    error = np.sqrt(mse(im1, im2))
-    
+def rmse(im1: np.ndarray, im2: np.ndarray) -> np.float64:
+    error: np.float64 = np.sqrt(mse(im1, im2))
     return error
 
 
@@ -27,21 +27,22 @@ Calculate the mean absolute difference per pixel between two images of the same 
 Lower MAE indicates more similar images.
 With perfect similarity, MAE=0.
 """
-def mae(im1, im2):
-    error = np.sum(np.abs(im1.astype(float) - im2.astype(float)))
+def mae(im1: np.ndarray, im2: np.ndarray) -> np.float64:
+    error: np.float64 = np.sum(np.abs(im1.astype(float) - im2.astype(float)))
     error /= im1.shape[0] * im1.shape[1]  
-    
     return error
+
 
 """
 Calculate the peak signal to noise ratio between two images of the same shape.
 Higher PSNR indicates higher-quality reconstruction (generally in the range of 30-50Db).
-With perfect similarity, PSNR=infinity
+With perfect similarity, PSNR=infinity.
+Reference: https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio
 """
-def psnr(im1, im2):
-    # https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio
-    error = (20 * np.log10(255)) - (10 * np.log10(mse(im1, im2)))
-    
+def psnr(im1: np.ndarray, im2: np.ndarray) -> np.float64:
+    with np.warnings.catch_warnings():
+        np.warnings.filterwarnings('ignore', r'RuntimeWarning: divide by zero encountered') # TODO: Fix Log10 issue (add machine epsilon?)
+        error: np.float64 = (20 * np.log10(255)) - (10 * np.log10(mse(im1, im2)))
     return error
     
 
@@ -51,7 +52,7 @@ Higher SSIM indicates more similarity between images (luminence, constrast, stru
 -1 to 1 scale.
 With perfect similarity, SSIM=1.
 """
-def ssim(im1, im2):
+def ssim(im1: np.ndarray, im2: np.ndarray) -> np.float64:
     # https://en.wikipedia.org/wiki/Structural_similarity
     mean1 = np.mean(im1)
     mean2 = np.mean(im2)
@@ -73,6 +74,5 @@ def ssim(im1, im2):
     c = ((2 * var1 * var2) + c2) / ((var1 ** 2) + (var2 ** 2) + c2)
     s = (cov + c3) / ((np.sqrt(var1) * np.sqrt(var2)) + c3)
 
-    error = l * c * s
-    
+    error: np.float64 = l * c * s
     return error
--- functions/utility.py
+++ functions/utility.py
@@ -1,47 +1,52 @@
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
+
+
 """
-Loads an image from path.
+Loads an image from path. Returns a blank Numpy Array if image is not found
 """
-def get_im(path):
-    im = cv2.imread(path)
-    return im
+def get_im(path: str) -> np.ndarray:
+    try:
+        im = cv2.imread('images/' + path)
+        return im
+    except:
+        return np.zeros((100, 100))
+    
+
 
 """
 Displays a BGR image.
 """
-def display_bgr(im):
+def display_bgr(im: np.ndarray) -> None:
     plt.imshow(im[:,:,[2,1,0]])
     plt.show()
+
 
 """
 Displays images relating to prepped upsampling (original, downsampled, prepped, upsampled).
 Assumes a BGR image format.
 """
-def display_prepped_upsampling_results(original_im, downsampled_im, prepped_im, upsampled_im):
+titles: list[str] = ['Original', 'Downsampled', 'Prepped', 'Upsampled']
+def display_prepped_upsampling_results(samples: list[np.ndarray]) -> None:
+    [original_im, downsampled_im, prepped_im, upsampled_im] = samples
+    
     fig, axs = plt.subplots(2, 2, figsize=(10, 10))
     fig.suptitle("Results for Prepped Upsampling", fontsize=16)
 
-    # Display the images
-    axs[0, 0].imshow(original_im[:,:,[2,1,0]])
-    axs[0, 0].set_title("Original")
-    
-    axs[0, 1].imshow(downsampled_im[:,:,[2,1,0]])
-    axs[0, 1].set_title("Downsampled")
-    
-    axs[1, 0].imshow(prepped_im[:,:,[2,1,0]])
-    axs[1, 0].set_title("Prepped")
-    
-    axs[1, 1].imshow(upsampled_im[:,:,[2,1,0]])
-    axs[1, 1].set_title("Upsampled")
+    # Display Images
+    for i, a in enumerate(axs.flatten()):
+        a.imshow(samples[i][:,:,[2,1,0]])
+        a.set_title(titles[i])
     
     plt.show()
     
+
 """
 Displays images relating to no-prep upsampling (original, downsampled, upsampled).
 Assumes a BGR image format.
 """
-def display_upsampling_results(original_im, upsampled_im):
+def display_upsampling_results(original_im: np.ndarray, upsampled_im: np.ndarray) -> None:
     fig, axs = plt.subplots(1, 2, figsize=(10, 5))
     fig.suptitle("Results for Upsampling", fontsize=16)
 
@@ -54,10 +59,11 @@ def display_upsampling_results(original_im, upsampled_im):
     
     plt.show()
     
+
 """
 Given an image, removes on row/column from either or both dimensions to ensure image shape is even.
 """
-def make_even_shape(im):
+def make_even_shape(im: np.ndarray) -> np.ndarray:
     if im.shape[0] % 2 != 0 and im.shape[1] != 0:
         return im[:im.shape[0]-1, :im.shape[1]-1]
     
